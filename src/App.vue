1df82c3a83b9374c8fbd14914fa9ff53d6470afd
Piotr <piotrwasak@outlook.com>
2022-07-20 18:57:19 +0200

    Exercise 8

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="app">
     <HelloWorld msg="Rick & Morty Characters" />
+    <VideoPlayer style="width: 800px; margin: 0 auto" src="/puppiness.mp4"></VideoPlayer>
   </div>
 </template>
 
 <script setup>
 import HelloWorld from "@/components/HelloWorld.vue";
+import VideoPlayer from "./components/VideoPlayer.vue";
 </script>
---
+++ src/components/VideoPlayer.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <video ref="video" loop muted playsinline :src="src" width="600"></video>
+    <button @click="handleControlBtn">Play / Pause</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useGlobalEvent } from "@/composables/useGlobalEvent";
+import { ref } from "vue";
+
+defineProps({
+  src: {
+    type: String,
+    required: true,
+  },
+});
+
+const video = ref<HTMLVideoElement>();
+
+function handleControlBtn() {
+  video.value.paused ? video.value.play() : video.value.pause();
+}
+
+useGlobalEvent("keypress", (event) => {
+  if (event.charCode === 32)
+    video.value.paused ? video.value.play() : video.value.pause();
+});
+</script>
+<style scoped></style>
