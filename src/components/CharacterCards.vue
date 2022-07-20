7988953c5aae730f4ccc7fb775dc68c4580f3cc3
Piotr <piotrwasak@outlook.com>
2022-07-20 16:51:21 +0200

    Exercise 2

--- src/components/CharacterCards.vue
+++ src/components/CharacterCards.vue
@@ -33,26 +33,25 @@
   </div>
 </template>
 
+<script setup>
+const characters = ref([]);
+const loadingState = ref(null);
+function fetchAllCharacters() {
+  loadingState.value = "loading";
+  axios.get("https://rickandmortyapi.com/api/character").then((response) => {
+    setTimeout(() => {
+      loadingState.value = "success";
+      characters.value = response.data.results;
+    }, 1000);
+  });
+}
+fetchAllCharacters();
+</script>
 <script>
 import axios from "axios";
 import orderBy from "lodash/orderby";
 import { ref } from "vue";
 export default {
-  setup() {
-    const characters = ref([]);
-    const loadingState = ref(null);
-    function fetchAllCharacters() {
-      loadingState.value = "loading";
-      axios.get("https://rickandmortyapi.com/api/character").then((response) => {
-        setTimeout(() => {
-          loadingState.value = "success";
-          characters.value = response.data.results;
-        }, 1000);
-      });
-    }
-    fetchAllCharacters();
-    return { characters, loadingState, fetchAllCharacters };
-  },
   data() {
     return {
       orderKey: "id",
